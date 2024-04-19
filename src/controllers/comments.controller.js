23f79233a5828aff17430a9d17c8c3b8f23df08f
syedaamish <aamish@try.city>
2024-04-19 16:20:40 +0530

    bugs

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -42,7 +42,7 @@ const tagUserInComment = catchAsync(async (commentId, userIdToTag, res) => {
 const createComment = catchAsync(async (req, res) => {
   const commentData = await commentsService.createComments(req.body);
   const SLACK_API_URL = 'https://slack.com/api/chat.postMessage';
-  const SLACK_TOKEN = config.slack.slackNotification;
+  const SLACK_TOKEN = process.env.SLACK_NOTIFICATION;
 
   const stdnt = await getStudentById(commentData.studentId);
   const sendSlackNotification = async (memberId, text, comment, student) => {
