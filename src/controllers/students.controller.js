80be8e8b29692aafc808cf77bba014d3e710ad9b
syedaamish <aamish@try.city>
2024-11-08 17:27:31 +0530

    bugs fixes

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -25,7 +25,17 @@ const createStudent = catchAsync(async (req, res) => {
 });
 
 const getStudents = catchAsync(async (req, res) => {
-  const filter = pick(req.query, ['name', 'role', 'qualified', 'degree', 'nationality', 'residence', 'status', 'stage']);
+  const filter = pick(req.query, [
+    'name',
+    'role',
+    'qualified',
+    'degree',
+    'nationality',
+    'residence',
+    'status',
+    'stage',
+    'ambassadorName',
+  ]);
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
   const result = await studentsService.queryStudents(filter, options);
   res.send(result);
